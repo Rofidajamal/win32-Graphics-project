a2792b17f14f3700633f76f2c2b7b7d842453b21
Rofidajamal <99247792+Rofidajamal@users.noreply.github.com>
2022-02-08 13:05:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,25 @@
+/*
+
+a. Change the background of window to be white 
+b. Try to change the shape of your window mouse 
+c. User must interact with window using mouse only 
+d. Try to make combination between your console and window 
+e. Give me an option to choose shape color before drawing from menu 
+f. Implement item to clear screen from shapes 
+g. Implement save function for all data in screen 
+h. Implement load function to load data from files 
+i. Implement line algorithms [DDA, Midpoint and parametric] 
+j. Implement Circle algorithms (Direct, Polari iterative Polar, midpoint and 
+modified Midpoint).
+k. Filling Circle with lines after taking filling quarter from user 
+l. Ellipse Algorithms [Direct and polar] 
+m. Clipping point and line
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+
+
 #if defined(UNICODE) && !defined(_UNICODE)
 #define _UNICODE
 #elif defined(_UNICODE) && !defined(UNICODE)
